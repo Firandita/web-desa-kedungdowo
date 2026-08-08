fb221493dff2657b8eff1b0165a0aa0ce22edfbe
fahmi <fahmifc8@gmail.com>
2026-08-08 21:18:31 +0700

    update sabtu08/08/2026

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./resources/**/*.js",
   ],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        'xs': '0 1px 2px 0 rgba(0, 0, 0, 0.05)',
+      },
+    },
   },
   plugins: [],
 }
